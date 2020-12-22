dfd7b65c062e28f5c4324152341624bb8a9607e2
Daniel Ordonez <daniel.ordonez@bizagi.com>
2020-12-21 20:58:20 -0500

    e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,16 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Root Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.contains('h1', ' Welcome to Torre Tasks ')
+  })
+})
+
+describe('Task view Test', () => {
+  it('Visits the app funcitonality', () => {
+    cy.visit('/tasks')
+    cy.contains('p', 'Information will only appear when both job and candidate are selected')
+    cy.contains('h1', 'Previously Stored Lists')
   })
 })
