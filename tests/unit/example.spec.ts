f265af7573b445c7aab7e8ff7498512810c0c86b
Daniel Ordonez <daniel.ordonez@bizagi.com>
2020-12-21 20:58:32 -0500

    unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,44 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, mount } from '@vue/test-utils'
+import JobCard from '@/components/JobCard.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('JobCard.vue', () => {
+  it('renders job title when passed', () => {
+    const objective = 'Freelance Thumbnail Specialist (TheGamer)'
+    const wrapper = mount(JobCard, {
+      propsData: { job:{
+        compensation: {data: {code: "range", currency: "USD$", minAmount: 15, maxAmount: 30, periodicity: "hourly"}},
+        context: {signaled: []},
+        deadline: "2020-12-30T04:30:00.000Z",
+        external: false,
+        id: "Owb4Pzro",
+        locations: ["United States", "Canada"],
+        members: [{subjectId: "218202", name: "Ana Varela", username: "AnaVarela"}],
+        objective: "Freelance Thumbnail Specialist (TheGamer)",
+        organizations: [{id: 553700, name: "Valnet", picture: "https://torre-media.s3-us-west-2.amazonaws.com/Valnet.png"}],
+        questions: [],
+        remote: true,
+        skills: [{name: "Thumbnail", experience: "1-plus-year"}, {name: "Photoshop", experience: "1-plus-year"}],
+        status: "open",
+        type: "freelance-gigs"
+      },
+      currentSelectedJob:{
+        compensation: {data: {code: "range", currency: "USD$", minAmount: 15, maxAmount: 30, periodicity: "hourly"}},
+        context: {signaled: []},
+        deadline: "2020-12-30T04:30:00.000Z",
+        external: false,
+        id: "Owb4Pzro",
+        locations: ["United States", "Canada"],
+        members: [{subjectId: "218202", name: "Ana Varela", username: "AnaVarela"}],
+        objective: "Freelance Thumbnail Specialist (TheGamer)",
+        organizations: [{id: 553700, name: "Valnet", picture: "https://torre-media.s3-us-west-2.amazonaws.com/Valnet.png"}],
+        questions: [],
+        remote: true,
+        skills: [{name: "Thumbnail", experience: "1-plus-year"}, {name: "Photoshop", experience: "1-plus-year"}],
+        status: "open",
+        type: "freelance-gigs"
+      } }
     })
-    expect(wrapper.text()).toMatch(msg)
+    const title =wrapper.find('p')
+    expect(title.text()).toMatch(objective)
   })
 })
